41de312e2f0547d0d82a665ff4c0f84f4cc2b4e9
Monica  Karasek <mlkarasek@gmail.com>
2018-08-15 04:31:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,47 +1,38 @@
+require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
-  set :views, Proc.new { File.join(root, "../views/") }
-
   configure do
-    enable :sessions
+    set :views, Proc.new { File.join(root, "../views/") }
+    enable :sessions unless test?
     set :session_secret, "secret"
   end
 
   get '/' do
-    erb :home
+    erb :index
   end
 
-  get '/registrations/signup' do
-    erb :'/registrations/signup'
-  end
-
-  post '/registrations' do
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-    @user.save
-    session[:user_id] = @user.id
-    redirect '/users/home'
-  end
-
-  get '/sessions/login' do
-    erb :'sessions/login'
-  end
-
-  post '/sessions' do
-      @user = User.find_by(email: params[:email], password: params[:password])
-      if @user
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && params[:password] == @user.password
       session[:user_id] = @user.id
-      redirect '/users/home'
+      redirect to '/account'
+    else
+      erb :error
     end
-    redirect '/sessions/login'
-  end 
+  end
 
-  get '/sessions/logout' do
+  get '/account' do
+    @user = User.find_by_id(session[:user_id])
+    if @user
+      erb :account
+    else
+      erb :error
+    end
+  end
+
+  get '/logout' do
     session.clear
-    redirect '/'
+    redirect to '/'
   end
 
-  get '/users/home' do
-    @user = User.find(session[:user_id])
-    erb :'/users/home'
-  end
+
 end
